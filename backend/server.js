86305e601e20c7283d1823dfbdd63d62ce6fc0b9
Dave Lafortune <davefortune527@gmail.com>
2025-06-04 12:21:03 +0200

    adding socket to the cloud

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      'http://localhost:5173', // local dev
+      // 'http://localhost:5173', // local dev
       'https://kitchen-bridge-75277.vercel.app' // production
     ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
